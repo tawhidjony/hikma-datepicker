779b4e84ba6cd8b3a851fdbb59a5fa961c9d3d47
tawhid <tawhid.jony.jhm@gmail.com>
2024-10-29 13:05:31 +0600

    fix: fix some file name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { default as UiButton } from './components/UiButton';
-export { default as UiDatepicker } from './components/UiDatePicker/UiDatepicker';
-export { default as UiInput } from './components/UiInput';
-export { default as UiSelect } from './components/UiSelect';
+export { default as UiDatepicker } from './components/UiBasicDatePicker/UiBasicDatePicker';
+
 
